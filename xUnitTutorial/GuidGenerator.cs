a1a0542659975b540b4eedd1ba8cfd2d42706ae0
Hamilton Monteiro Neto <hamiltonmneto@gmail.com>
2021-06-07 00:49:29 -0300

    xUnit examples

---
+++ xUnitTutorial/GuidGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xUnitTutorial
+{
+    public class GuidGenerator
+    {
+        public Guid RandomGuid { get; } = Guid.NewGuid();
+    }
+}
---
+++ xUnitTutorial/GuidGeneratorTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace xUnitTutorial
+{
+    [CollectionDefinition("Guid generator")]
+    public class GuidGeneratorDefinition : ICollectionFixture<GuidGenerator> { }
+    [Collection("Guid generator")]
+    public class GuidGeneratorTestsOne
+    {
+        private readonly GuidGenerator _guidGenerator;
+        private readonly ITestOutputHelper _output;
+
+        public GuidGeneratorTestsOne(ITestOutputHelper output, GuidGenerator guidGenerator)
+        {
+            _output = output;
+            _guidGenerator = guidGenerator;
+        }
+
+        [Fact]
+        public void GuidTestOne()
+        {
+            var guid = _guidGenerator.RandomGuid;
+            _output.WriteLine($"The guid was: {guid}");
+        }
+
+        [Fact]
+        public void GuidTestTwo()
+        {
+            var guid = _guidGenerator.RandomGuid;
+            _output.WriteLine($"The guid was: {guid}");
+        }
+    }
+
+    public class GuidGeneratorTestsTwo
+    {
+        private readonly GuidGenerator _guidGenerator;
+        private readonly ITestOutputHelper _output;
+        public GuidGeneratorTestsTwo(ITestOutputHelper output, GuidGenerator guidGenerator)
+        {
+            _output = output;
+            _guidGenerator = guidGenerator;
+        }
+
+        [Fact]
+        public void GuidTestOne()
+        {
+            var guid = _guidGenerator.RandomGuid;
+            _output.WriteLine($"The guid was: {guid}");
+        }
+
+        [Fact]
+        public void GuidTestTwo()
+        {
+            var guid = _guidGenerator.RandomGuid;
+            _output.WriteLine($"The guid was: {guid}");
+        }
+    }
+}
